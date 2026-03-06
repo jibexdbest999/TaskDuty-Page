1db2202b45bfb56cb355964bf031fa5e48e46a91
jibexdbest999 <jibexdbest356@gmail.com>
2026-03-06 08:53:04 +0100

    loadings

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../Components/NavBar";
 import Body from "../Components/Body";
-import { HashLoader } from "react-spinners"
+import { HashLoader } from "react-spinners";
 
 export default function HomePage() {
   const [pageIsLoading, setPageIsLoading] = useState(true);
@@ -11,10 +11,15 @@ export default function HomePage() {
     return () => clearTimeout(timer);
   }, []);
 
-  if (pageIsLoading) return <div className='flex flex-col mx-auto items-center justify-center h-screen'>
-      <HashLoader color="#974FD0" size={55} />
-      <p className='text-[18px] lg:text-[30px] pt-2 font-semibold text-[#974FD0]'>Loading...</p>
-    </div>
+  if (pageIsLoading)
+    return (
+      <div className="flex flex-col mx-auto items-center justify-center h-screen">
+        <HashLoader color="#974FD0" size={55} />
+        <p className="text-[18px] lg:text-[30px] pt-2 font-semibold text-[#974FD0]">
+          Loading...
+        </p>
+      </div>
+    );
   return (
     <div>
       <NavBar />
