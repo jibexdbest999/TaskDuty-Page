87502ee65ad2ea208f98afe79b402d4fa1e5bb63
jibexdbest999 <jibexdbest356@gmail.com>
2026-06-19 23:19:18 +0100

    extras

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/auth";
+const API = "https://taskduty-server-qloc.onrender.com/auth";
 
 export const loginUsers = (userData) =>
   axios.post(`${API}/login`, userData);
